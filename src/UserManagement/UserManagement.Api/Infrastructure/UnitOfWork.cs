a8f710242ab8ebfef70fb5dacac9caf2174cc9ac
Henning Meyer <henning@sealedcore.tech>
2023-10-20 20:47:23 +0200

    feat: add UnitOfWork

---
+++ src/UserManagement/UserManagement.Api/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using UserManagement.Api.Domain.Core.Interfaces;
+using UserManagement.Api.Infrastructure.Data;
+
+namespace UserManagement.Api.Infrastructure;
+
+internal sealed class UnitOfWork
+    : IUnitOfWork
+{
+    private readonly UserManagementContext _dbContext;
+
+    public UnitOfWork(UserManagementContext context)
+        => _dbContext = context;
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        => await _dbContext.SaveChangesAsync(cancellationToken);
+}
