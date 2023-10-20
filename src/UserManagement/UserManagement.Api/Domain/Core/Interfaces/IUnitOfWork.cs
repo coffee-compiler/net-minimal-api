a55bbec6c1e51d99bce3053a375b9d67792ef840
Henning Meyer <henning@sealedcore.tech>
2023-10-20 20:44:33 +0200

    feat: add IUnitOfWork interface

---
+++ src/UserManagement/UserManagement.Api/Domain/Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using UserManagement.Api.Domain.Repositories;
+
+namespace UserManagement.Api.Domain.Core.Interfaces;
+
+internal interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
