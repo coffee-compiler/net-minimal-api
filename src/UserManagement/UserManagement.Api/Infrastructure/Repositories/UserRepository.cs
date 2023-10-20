d18f004d1005119e0ff7cec87171a1d0bd22d5bd
Henning Meyer <henning@sealedcore.tech>
2023-10-20 20:46:05 +0200

    feat: add UserRepository

---
+++ src/UserManagement/UserManagement.Api/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using UserManagement.Api.Domain.Entities;
+using UserManagement.Api.Domain.Repositories;
+using UserManagement.Api.Infrastructure.Data;
+
+namespace UserManagement.Api.Infrastructure.Repositories;
+
+internal sealed class UserRepository
+    : GenericRepository<User>, IUserRepository
+{
+    public UserRepository(UserManagementContext context)
+        : base(context)
+    {
+    }
+}
