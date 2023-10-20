27a8fb57a2d995fb60aef0a27a65a9bf2e87f34c
Henning Meyer <henning@sealedcore.tech>
2023-10-20 20:50:14 +0200

    feat: add services for DI

--- src/UserManagement/UserManagement.Api/Program.cs
+++ src/UserManagement/UserManagement.Api/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Api.Application;
+using UserManagement.Api.Application.Services;
+using UserManagement.Api.Domain.Core.Interfaces;
+using UserManagement.Api.Domain.Repositories;
+using UserManagement.Api.Infrastructure;
+using UserManagement.Api.Infrastructure.Data;
+using UserManagement.Api.Infrastructure.Repositories;
+using UserManagement.Api.Presentation.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<UserManagementContext>(
+    options => options.UseInMemoryDatabase("UserManagementDb"));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
@@ -9,5 +26,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+UserEndpoints.Map(app);
 
 app.Run();
