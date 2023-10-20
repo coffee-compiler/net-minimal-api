e28357c15943ff0ff28de3bb3dcb3b7f2ce0ccb6
Henning Meyer <henning@sealedcore.tech>
2023-10-20 20:44:20 +0200

    feat: add IUserRepository interface

---
+++ src/UserManagement/UserManagement.Api/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using UserManagement.Api.Domain.Core.Interfaces;
+using UserManagement.Api.Domain.Entities;
+
+namespace UserManagement.Api.Domain.Repositories;
+
+internal interface IUserRepository
+    : IGenericRepository<User>
+{
+}
