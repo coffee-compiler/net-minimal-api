edec6c3db504eea72c1a387a51a6a2d331df3131
Henning Meyer <henning@sealedcore.tech>
2023-10-20 20:43:11 +0200

    feat: add user entity

---
+++ src/UserManagement/UserManagement.Api/Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagement.Api.Domain.Entities;
+
+internal sealed record User
+{
+    public Guid Id { get; init; }
+    
+    public string Firstname { get; init; } = string.Empty;
+
+    public string Lastname { get; init; } = string.Empty;
+}
