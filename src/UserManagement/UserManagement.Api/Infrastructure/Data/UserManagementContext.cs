af5cd8fb682f8a7545c6c5b9ad2e81945abca64e
Henning Meyer <henning@sealedcore.tech>
2023-10-20 20:45:29 +0200

    feat: add UserManagementContext

---
+++ src/UserManagement/UserManagement.Api/Infrastructure/Data/UserManagementContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Api.Domain.Entities;
+
+namespace UserManagement.Api.Infrastructure.Data;
+
+internal sealed class UserManagementContext
+    : DbContext
+{
+    public UserManagementContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<User>? Users { get; set; }
+}
